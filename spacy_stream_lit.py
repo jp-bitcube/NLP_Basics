6ce861fad70db79ca4903d062b3743aad46860ce
jp-bitcube <jp@bitcube.tech>
2021-06-15 17:54:19 +0200

    Update markdown

--- spacy_stream_lit.py
+++ spacy_stream_lit.py
@@ -1,5 +1,5 @@
 import spacy_streamlit
 
-models = ["en_core_web_sm", "en_core_web_md"]
-default_text = "I love coding in Python and using NLP packages like SpaCy"
+models = ["en_core_web_sm", "en_core_web_md", "en_core_web_lg"]
+default_text = "Charlie, loves coding in Python and using NLP packages like SpaCy"
 spacy_streamlit.visualize(models, default_text)
